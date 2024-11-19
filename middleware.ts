8eac27f8004049a5ce1e920d5e9c79d47276f596
Katka1111 <ksnopkova@novis.eu>
2024-11-19 12:42:05 +0100

    9

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,14 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export const middleware = clerkMiddleware((auth, request) => {
+export default clerkMiddleware((auth, request) => {
   if (!request.url.includes('/sign-in') && 
       !request.url.includes('/sign-up') && 
       !request.url.includes('/') && 
       !request.url.includes('/api/webhook')) {
-    return auth.protect();
+    return auth.protect().then(() => NextResponse.next());
   }
+  return NextResponse.next();
 });
 
 export const config = {
