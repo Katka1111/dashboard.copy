5ead3ae051c8d7f56a3f1a31da5808013457042c
Katka1111 <ksnopkova@novis.eu>
2024-11-19 12:38:05 +0100

    8

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,22 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
-
-export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect();
+export const middleware = clerkMiddleware((auth, request) => {
+  if (!request.url.includes('/sign-in') && 
+      !request.url.includes('/sign-up') && 
+      !request.url.includes('/') && 
+      !request.url.includes('/api/webhook')) {
+    return auth.protect();
   }
 });
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!_next/static|_next/image|favicon.ico).*)",
   ],
 };
